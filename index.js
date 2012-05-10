17c7b5fcc130d2138d19d15e7f4bec9d7c385391
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2012-05-11 00:59:14 +0200

    Major improvements + new methods

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var formats = exports.formats = {};
+var fs = require('fs')
+  , path = require('path')
+  , files = fs.readdirSync('./lang')
+  , langs = {}
+  , lang
+
+files.forEach(function (filename) {
+  var language = filename.replace(/\.json$/, '');
+  
+  langs[language] = require(path.resolve('lang', filename));
+});
 
 var units = exports.units = {
-    second  : 1000
-  , minute  : 60000
-  , day     : 86400000
-  , month   : 2592000000
-  , year    : 31536000000
+    years   : 31536000000
+  , months  : 2592000000
+  , weeks   : 604800000
+  , days    : 86400000
+  , hours   : 3600000
+  , minutes : 60000
+  , seconds : 1000
 };
+
+var formats = exports.formats = {};
+
+// Set the default language
+var lang = langs['en-us'];
+
 
 /**
  * Returns a date as a formatted string
@@ -17,39 +35,54 @@ var units = exports.units = {
  */
 
 exports.format = function (date, format) {
-  var tokens = /Y{2,4}|[MDhms]{1,2}|"[^"]*"|'[^']*'/g;
+  var tokens = /Y{2,4}|[Md]{1,4}|[DHhms]{1,2}|[Aa]|"[^"]*"|'[^']*'/g
+    , date = toObject(date);
   
   format = formats[format] || format;
-  
+
   return format.replace(tokens, function (part) {
     switch (part) {
       case 'YYYY':
-        return date.getFullYear();
+        return pad(date.Y, 3);
       case 'YY':
-        return date.getYear();
+        return pad(date.Y);
+      case 'MMMM':
+      case 'MMM':
+        return lang[part][date.M];
       case 'MM':
-        return padZero(date.getMonth() + 1);
+        return pad(date.M + 1);
       case 'M':
-        return date.getMonth() + 1;
+        return date.M + 1;
       case 'DD':
-        return padZero(date.getDate());
+        return pad(date.D);
       case 'D':
-        return date.getDate();
+        return date.D;
+      case 'dddd':
+      case 'ddd':
+        return lang[part][date.d];
+      case 'A':
+        return date.A;
+      case 'a':
+        return date.a
+      case 'H':
+        return date.H
+      case 'HH':
+        return pad(date.H);
       case 'hh':
-        return padZero(date.getHours());
+        return pad(date.h);
       case 'h':
-        return date.getHours();
+        return date.h
       case 'mm':
-        return padZero(date.getMinutes());
+        return pad(date.m);
       case 'm':
-        return date.getMinutes();
+        return date.m
       case 'ss':
-        return padZero(date.getSeconds());
+        return pad(date.s);
       case 's':
-        return date.getSeconds();
+        return date.s;
       default:
-        return part.slice(1, part.length - 1);
-    };
+        return part.slice(1, -1);
+    }
   });
 };
 
@@ -66,18 +99,21 @@ exports.parse = function (string, format) {
     , index
     , stringLength
     , tokenLength
+    , part
     , parts = {}
     , offset = 0;
   
   format = formats[format] || format;
-  
-  format = format.replace(/"[^"]*"|'[^']*'/g, function (part) {
-    string = string.replace(part.slice(1, part.length - 1), '');
+
+  // Strip the string from the escaped parts of the format
+  format = format.replace(/"[^"]*"|'[^']*'/g, function (str) {
+    string = string.replace(str.slice(1, -1), '');
     return '';
   });
   
-  tokens = format.match(/Y{2,4}|[MDhms]{1,2}/g);
-  stringLength = string.length
+  // Generate tokens
+  tokens = format.match(/Y{2,4}|[Md]{1,4}|[DHhms]{1,2}|[Aa]/g);
+  stringLength = string.length;
   
   for (var i = 0; i < tokens.length; i++) {
     index = format.indexOf(tokens[i]) + offset;
@@ -85,45 +121,117 @@ exports.parse = function (string, format) {
     if (index - offset < 0) continue;
     
     tokenLength = tokens[i].length;
-    parts[tokens[i][0]] = string.substr(index, tokenLength);
+    part = string.substr(index, tokenLength);
     index += tokenLength - 1;
+
+    // Remove characters that are not part of the format
+    // e.g, MMMM > May
+    part = part.replace(/\W+.*/, function (str) {
+      index -= str.length;
+      return '';
+    });
     
+    // Looks ahead for characters beyond the
+    // specified format, e.g, D > 9
     while (++index < stringLength) {
-      if (!(/[0-9]/).test(string[index]))
+      if (!(/\d|\w/).test(string[index]))
         break;
       
-      parts[tokens[i][0]] += string[index];
-      offset++;
-    }  
+      part += string[index];
+    }
+    
+    offset += part.length - tokenLength;
+    
+    if (/[Md]{3,4}/.test(tokens[i])) {
+      part = lang[tokens[i]].indexOf(part) || '';
+      if (tokens[i][0] === 'M') part++
+    }
+    
+    parts[tokens[i][0]] = part;
   }
   
-  return new Date('Y-M-D,h:m:s'.replace(/\w/g, function (part) {
-    return parts[part] || 0;
-  }));
+  return toDate(parts);
 };
 
 /**
- * Returns the time until a date in a given unit
+ * Converts from 12-hours to 24-hours
  *
- * @param {Date} date
- * @param {String} unit
+ * @param {String|int} hours
+ * @param {String} abbr
  * @return {int}
+ * @api public
  */
 
-exports.until = function (date, unit) {
-  return Math.round((end.valueOf() - Date.now()) / units[unit]);
+exports.fromAmPm = function (hours, abbr) {
+  hours = parseInt(hours, 10);
+  abbr = abbr
+    .toLowerCase()
+    .replace(/\./g, '');
+
+  if (abbr == 'pm' && hours < 12)
+    return hours + 12;
+  
+  return hours;
 };
 
 /**
- * Returns the time since a date in a given unit
+ * Converts from 24-horus to 12-hours
+ *
+ * @param {String|Date|int} hours
+ * @return {int}
+ * @api public
+ */
+
+exports.toAmPm = function (hours) {
+  if (hours instanceof Date)
+    hours = hours.getHours();
+  else
+    hours = parseInt(hours, 10);
+  
+  if (hours > 12)
+    return hours - 12;
+  
+  return hours;
+};
+
+/**
+ * Returns either "am" or "pm", depending on the input
+ *
+ * @param {String|Date|int} hours
+ * @return {int}
+ * @api public
+ */
+
+exports.isAmPm = function (hours) {
+  if (hours instanceof Date)
+    hours = hours.getHours();
+  else
+    hours = parseInt(hours, 10);
+  
+  return hours >= 12 ? 'pm' : 'am';
+}
+
+/**
+ * Answers the question "when?"
+ * Returned unit can be specified or omitted
  *
  * @param {Date} date
  * @param {String} unit
- * @return {int}
+ * @return {String}
+ * @api public
  */
 
-exports.since = function (date, unit) {
-  return - exports.daysUntil(date, unit);
+exports.when = function (date, unit) {
+  var diff = Date.now() - date.valueOf()
+    , result = exports.inUnit(diff, unit)
+    , time = 'present';
+    
+  if (result.value < 0) time = 'future';
+  else if (result.value > 0) time = 'past';
+  
+  result.value = Math.abs(result.value);
+  
+  return parseString(lang.time[time], parseString(result.unit, result.value));
 };
 
 /**
@@ -131,56 +239,138 @@ exports.since = function (date, unit) {
  *
  * @param {Date} date
  * @return {int}
+ * @api public
  */
 
-exports.between = function (start, end, unit) {
-  return Math.abs(Math.round((end.valueOf() - start.valueOf()) / units.day));
+exports.diff = function (start, end, unit) {
+  var diff = start.valueOf() - end.valueOf()
+    , result = inUnit(diff, unit);
+  
+  return parseString(result.unit, result.value);
 };
 
 /**
- * Returns the number of days in a month
+ * Calculates a timeframe in a given unit.
+ * If unit is omitted, it will try to find the one that's best suited
+ * Returns an Object containing the value and the unit
  *
- * @param {Date|int} year
- * @param {int} [month]
+ * @param {int} ms
+ * @param {String} [unit]
+ * @return {Object}
+ * @api public
  */
 
-exports.daysInMonth = function (year, month) {
-  var date = year;
+exports.inUnit = function (ms, unit) {
+  var keys = Object.keys(units)
+    , result = {}
+    , value
+    
+  if (!unit || !units[unit]) {
+    for (var i = 0; i < keys.length; i++) {
+      value = ms / units[unit];
+      unit = keys[i];
+      if (Math.abs(value) >= 1)
+        break;
+    }
+  } else {
+    value = ms / units[unit];
+  }
   
-  if (!(date instanceof Date))
-    date = new Date(year, month);
+  result.value = Math.round(value);
   
-  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  if (Math.abs(value) > 1 && lang.units[unit][1])
+    result.unit = lang.units[unit][1];
+  else
+    result.unit = lang.units[unit][0];
+    
+  return result;
 };
 
 /**
- * Returns all the dates in a month
+ * Sprinf-like method for parsing output strings
  *
- * @param {Date|int} year
- * @param {int} [month]
- * @return {Array}
+ * @param {String}
+ * @return {String}
+ * @api private
  */
 
-exports.getFullMonth = function (year, month) {
-  var dates = []
-    , date = year;
-  
-  if (!(date instanceof Date))
-    date = new Date(year, month);
-  
-  for (var d = 1; d <= exports.daysInMonth(date); d++)
-    dates.push(new Date(date.getFullYear(), date.getMonth(), d));
-  
-  return dates;
+function parseString (string) {
+  var args = [].slice.call(arguments, 1)
+    , offset = 0;
+
+  return string.replace(/%d([0-9])*/g, function (s, n) {
+    n = (n || 0 + offset);  
+    offset++;
+    
+    return args[n];
+  });
 };
 
 /**
- * Pads out a number to two digits
+ * Pads a number with zeros
  *
  * @param {int} number
+ * @param {int} [zeros]
  * @return {String}
+ * @api private
  */
 
-function padZero (number) {
-  return number < 10 ? '0' + number : '' + number;
+function pad (number, zeros) {
+  return number < Math.pow(10, zeros || 1) 
+    ? '0' + number 
+    : '' + number;
 }
+
+/**
+ * Converts an Object to a date
+ *
+ * @param {Object} parts
+ * @return {Date}
+ * @api private
+ */
+ 
+function toDate (obj) {
+  var date = new Date(0)
+    , abbr = obj.A || obj.a
+  
+  // Handle AM/PM
+  if (abbr && obj.h)
+    obj.H = exports.fromAmPm(obj.h, abbr);
+  
+  date.setFullYear(obj.Y || 0);
+  date.setMonth((obj.M || 1) - 1);
+  date.setDate(obj.D || 0);
+  date.setHours(obj.H || 0);
+  date.setMinutes(obj.m || 0);
+  date.setSeconds(obj.s || 0);
+  
+  return date;
+};
+
+/**
+ * Converts a date to an Object
+ *
+ * @param {Date} date
+ * @return {Object}
+ * @api private
+ */
+
+function toObject (date) {
+  var obj = {
+      Y: date.getFullYear()
+    , y: date.getYear()
+    , M: date.getMonth()
+    , D: date.getDate()
+    , d: date.getDay()
+    , H: date.getHours()
+    , h: date.getHours()
+    , m: date.getMinutes()
+    , s: date.getSeconds()
+  };
+  
+  obj.h = exports.toAmPm(obj.H);
+  obj.a = exports.isAmPm(obj.H);
+  obj.A = obj.a.toUpperCase();
+  
+  return obj;
+};
--- test.js
+++ test.js
@@ -1,22 +1,54 @@
 var dateable = require('./')
-  , assert = require('assert')
-  , date = new Date(2012, 04, 17, 21, 10, 35)
-  , formatted
-  , parsed
+  , assert = require('assert');
 
-dateable.formats.test = '"Testing date" D/M-YYYY, "and time" hh:mm:ss';
-formatted = dateable.format(date, 'test');
-parsed = dateable.parse(formatted, 'test');
+dateable.formats.test = '"Testing date" ddd D. MM YYYY, "and time" hh:mm:ss a';
 
-assert.equal(formatted, 'Testing date 17/5-2012, and time 21:10:35');
-assert.notStrictEqual(parsed, date);
-assert.equal(parsed.getFullYear(), date.getFullYear());
-assert.equal(parsed.getMonth(), date.getMonth());
-assert.equal(parsed.getDate(), date.getDate());
-assert.equal(parsed.getHours(), date.getHours());
-assert.equal(parsed.getMinutes(), date.getMinutes());
-assert.equal(parsed.getSeconds(), date.getSeconds());
-assert.equal(dateable.between(date, parsed, 'day'), 0);
-assert.equal(dateable.between(date, new Date(2012, 04, 18, 21, 10, 40), 'day'), 1);
 
-console.log('All tests passed');
+var tests = module.exports = {
+  'test format': function () {
+    var date = new Date(2012, 04, 17, 21, 10, 35)
+      , expected = 'Testing date Thu 17. 05 2012, and time 09:10:35 pm';
+
+    assert.equal(dateable.format(date, 'test'), expected);
+  },
+  
+  'test parse': function () {
+    var date = ranDate()
+      , expected = date.valueOf();
+      
+    date = dateable.format(date, 'test');
+    assert.equal(dateable.parse(date, 'test').valueOf(), expected);
+  },
+  
+  'test when': function () {
+    var date = new Date();
+    console.log(date);
+    date.setHours(date.getHours() - 1);
+    console.log(date);
+    assert.equal(dateable.when(date), 'an hour ago');
+  }
+};
+
+
+function ranDate () {
+  var date = new Date();
+  
+  date.setFullYear(2012);
+  date.setMonth(ranNo(2, 4));
+  date.setDate(ranNo(28, 1));
+  date.setHours(ranNo(24));
+  date.setMinutes(ranNo(59));
+  date.setSeconds(ranNo(59));
+  date.setMilliseconds(0);
+
+  return date;
+};
+
+function ranNo (max, offset) {
+  return Math.floor(Math.random() * (max + 1)) + (offset || 0);
+}
+
+for (var i in tests)
+ tests[i]();
+ 
+console.log('all tests passed');
